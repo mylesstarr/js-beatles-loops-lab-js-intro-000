0685cb66d9ce0809140a959d123b90b4fc2bcdf8
Myles Starr <mylesstarr@gmail.com>
2021-03-25 01:30:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const beatlesWithInstruments = []
+
+  for (let num = 0, l = musicians.length; num < l; num++) {
+    beatlesWithInstruments.push(`${musicians[num]} plays ${instruments[num]}`)
+  }
+
+  return beatlesWithInstruments
+}
+
+function johnLennonFacts(facts) {
+  const lennonFactsEx = []
+
+  let num = 0
+  while (num < facts.length) {
+    lennonFactsEx.push(`${facts[num]}!!!`)
+    num++
+  }
+
+  return lennonFactsEx
+}
+
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+ return arr
+
+}
